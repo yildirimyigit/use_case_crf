f9741c1d1b38d3265f9f6df7a659d64c17aca06b
Yigit Yildirim <yigit.yildirim@bogazici.edu.tr>
2026-02-25 15:37:16 +0100

    feat: Compile ros ws when starting the container

--- run_gui.sh
+++ run_gui.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 xhost +SI:localuser:root
 
-# Added space after --name, added missing \ after DISPLAY, and used $(pwd) for absolute host path
+# Mount the entire ros2_ws and append the build-and-run command
 docker run --rm -it --gpus all --net=host --name "ur_simulation_env" \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -v $(pwd)/ros2_ws/src:/ros2_ws/src:rw \
-  ur_simulation_env
+  -v $(pwd)/ros2_ws:/ros2_ws:rw \
+  ur_simulation_env \
+  bash -c "colcon build --symlink-install && source install/setup.bash && exec bash"
