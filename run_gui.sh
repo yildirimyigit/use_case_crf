29664770f854a498ebac1460b9549582922c458c
Yigit Yildirim <yigit.yildirim@bogazici.edu.tr>
2026-02-27 09:56:45 +0100

    feat: Build 3rd party labs in the first run

--- run_gui.sh
+++ run_gui.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 xhost +SI:localuser:root
 
-# Mount the entire ros2_ws and append the build-and-run command
 docker run --rm -it --gpus all --net=host --name "ur_simulation_env" \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
   -v $(pwd)/ros2_ws:/ros2_ws:rw \
   ur_simulation_env \
-  bash -c "colcon build --symlink-install && source install/setup.bash && exec bash"
+  bash -c "colcon build --symlink-install --packages-skip cartesian_controller_simulation cartesian_controller_tests --cmake-args -DCMAKE_BUILD_TYPE=Release && source install/setup.bash && exec bash"
