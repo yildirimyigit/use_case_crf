2099e7a51a17973273e4335b734732f1e136c89e
Yigit Yildirim <yigit.yildirim@bogazici.edu.tr>
2026-02-27 10:00:13 +0100

    feat: Replace ur_launch with custom launch to use custom parameters, required for the gripper

--- ros2_ws/src/use_case_sim/launch/task_env.launch.py
+++ ros2_ws/src/use_case_sim/launch/task_env.launch.py
@@ -1,48 +1,95 @@
 import os
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, AppendEnvironmentVariable, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
-    ur_sim_dir = get_package_share_directory('ur_simulation_gazebo')
     custom_pkg_dir = get_package_share_directory('use_case_sim')
-
+    xacro_file = os.path.join(custom_pkg_dir, 'urdf', 'task_env.urdf.xacro')
+    peg_urdf = os.path.join(custom_pkg_dir, 'urdf', 'peg.urdf')
     rviz_config = os.path.join(custom_pkg_dir, 'rviz', 'default.rviz')
     custom_controllers = os.path.join(custom_pkg_dir, 'config', 'task_controllers.yaml')
 
-    ur_launch = IncludeLaunchDescription(
+    robotiq_install_dir = get_package_prefix('robotiq_description')
+    gazebo_mesh_fix = AppendEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=[os.path.join(robotiq_install_dir, 'share'), ':']
+    )
+
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name='xacro')]), ' ', xacro_file,
+        ' simulation_controllers:=', custom_controllers
+    ])
+    robot_description = {'robot_description': robot_description_content}
+
+    robot_state_pub_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[robot_description]
+    )
+
+    gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(ur_sim_dir, 'launch', 'ur_sim_control.launch.py')
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
         ),
-        launch_arguments={
-            'ur_type': 'ur10',
-            'description_package': 'use_case_sim',
-            'description_file': 'task_env.urdf.xacro',
-            'launch_rviz': 'false',
-            # Override controllers with our combined arm+gripper yaml
-            'simulation_controllers': custom_controllers
-        }.items()
+    )
+
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'ur10_peg_task'],
+        output='screen'
+    )
+
+    spawn_peg = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-file', peg_urdf,
+            '-entity', 'blue_peg',
+            '-x', '0.7',
+            '-y', '0.1',
+            '-z', '1.0',
+        ],
+        output='screen'
     )
 
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
-        name='rviz2',
-        output='screen',
         arguments=['-d', rviz_config]
     )
 
-    # Spawn the gripper controller alongside the arm controllers
-    gripper_controller_spawner = Node(
+    joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+    )
+
+    joint_trajectory_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_trajectory_controller', '--controller-manager', '/controller_manager'],
+    )
+
+    robotiq_gripper_controller = Node(
         package='controller_manager',
         executable='spawner',
         arguments=['robotiq_gripper_controller', '--controller-manager', '/controller_manager'],
     )
 
     return LaunchDescription([
-        ur_launch,
+        gazebo_mesh_fix,
+        robot_state_pub_node,
+        gazebo,
+        spawn_robot,
+        spawn_peg,
         rviz_node,
-        gripper_controller_spawner
+        RegisterEventHandler(OnProcessExit(target_action=spawn_robot, on_exit=[joint_state_broadcaster])),
+        RegisterEventHandler(OnProcessExit(target_action=joint_state_broadcaster, on_exit=[joint_trajectory_controller, robotiq_gripper_controller])),
     ])
